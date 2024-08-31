02cad407a481750ce47734561247490c0a65ce6d
root <root@warewulf.hpc.uco.edu>
2024-08-31 14:48:12 -0500

    Added system uptime to MotD

--- motd/system-info-motd.sh
+++ motd/system-info-motd.sh
@@ -7,24 +7,32 @@
 
 diskusage=$(df -H -x squashfs -x tmpfs -x devtmpfs --total|grep /$)
 
+#System uptime
+uptime=`cat /proc/uptime | cut -f1 -d.`
+upDays=$((uptime/60/60/24))
+upHours=$((uptime/60/60%24))
+upMins=$((uptime/60%60))
+upSecs=$((uptime%60))
+
 echo "###############################################"
-echo -e " Hostname: \033[1;33m$(hostname)\033[0m"
-echo " Distro: $(hostnamectl | grep -Po 'Operating System: \K.*')"
-echo " Kernel: $(hostnamectl | grep -Po 'Kernel: \K.*')"
+echo -e " Hostname..........: \033[1;33m$(hostname)\033[0m"
+echo " Distro............: $(hostnamectl | grep -Po 'Operating System: \K.*')"
+echo " Kernel............: $(hostnamectl | grep -Po 'Kernel: \K.*')"
 echo
-echo " Machine: $(hostnamectl | grep -Po 'Hardware Vendor: \K.*') $(hostnamectl | grep -Po 'Hardware Model: \K.*')"
-echo " CPU: $(cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f2)"
-echo " Disk: $(echo $diskusage | awk '{ a = $2 } END { print a }'|sed 's/G/ G/'|sed 's/T/ T/')"
-echo " Memory: $(free -m | head -n 2 | tail -n 1 | awk {'print $2'}) M"
-echo " Swap: $(free -m | tail -n 1 | awk {'print $2'}) M"
+echo " Machine...........: $(hostnamectl | grep -Po 'Hardware Vendor: \K.*') $(hostnamectl | grep -Po 'Hardware Model: \K.*')"
+echo " CPU...............: $(cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f2)"
+echo " Disk..............: $(echo $diskusage | awk '{ a = $2 } END { print a }'|sed 's/G/ G/'|sed 's/T/ T/')"
+echo " Memory............: $(free -m | head -n 2 | tail -n 1 | awk {'print $2'}) M"
+echo " Swap..............: $(free -m | tail -n 1 | awk {'print $2'}) M"
+echo " System uptime.....: $upDays days $upHours hours $upMins minutes $upSecs seconds"
 echo
 
 if which ip >/dev/null
 then
-echo " IP V4 Networks"
+echo " IP V4 Networks:"
 ip -o -4 -br a|grep -v '^lo'|sed 's/^/ /'|sed 's/\/..//'
 echo
-echo " IP V6 Networks"
+echo " IP V6 Networks:"
 ip -o -6 -br a|grep -v '^lo'|sed 's/^/ /'|sed 's/\/..//'
 fi
 echo "###############################################"
