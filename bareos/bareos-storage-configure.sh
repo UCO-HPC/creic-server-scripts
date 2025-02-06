46774cf2a430452e0d4b991c9f15ae7446469aba
Ryan Maher <rmaher@uco.edu>
2025-02-06 16:56:20 -0600

    Revert "Fixing backup directory location"
    This reverts commit 377fddc08729ccdcb2f2fdfa73779e020aca9baf.

--- bareos/bareos-storage-configure.sh
+++ bareos/bareos-storage-configure.sh
@@ -8,7 +8,7 @@ read servername
 echo 'Device {
   Name = '"${servername^}"'FileStorage
   Media Type = File
-  Archive Device = /mnt/backup/bareos/
+  Archive Device = /mnt/backup/bareos/'"${servername}"'
   LabelMedia = yes;                   # lets Bareos label unlabeled media
   Random Access = yes;
   AutomaticMount = yes;               # when device opened, read it
@@ -18,9 +18,9 @@ echo 'Device {
 }
 ' > /etc/bareos/bareos-sd.d/device/"${servername^}"FileStorage.conf
 
-# mkdir /mnt/backup/bareos/${servername}
-# chown -R bareos:bareos /mnt/backup/bareos/${servername}
+mkdir /mnt/backup/bareos/${servername}
+chown -R bareos:bareos /mnt/backup/bareos/${servername}
 systemctl restart bareos-sd
 
-echo "Backup directory set to /mnt/backup/bareos/"
+echo "Backup directory created at /mnt/backup/bareos/${servername}"
 echo "Storage device configured at /etc/bareos/bareos-sd.d/device/${servername^}FileStorage.conf"
